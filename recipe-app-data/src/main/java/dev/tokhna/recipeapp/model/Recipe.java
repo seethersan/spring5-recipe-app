8f1e6e6ec4656b29678dd4a504aae6cfb68f1821
seethersan <carlos_jcez@hotmail.com>
2020-01-20 11:39:31 -0500

    created Category and added relationship with Recipe

--- recipe-app-data/src/main/java/dev/tokhna/recipeapp/model/Recipe.java
+++ recipe-app-data/src/main/java/dev/tokhna/recipeapp/model/Recipe.java
@@ -23,6 +23,11 @@ public class Recipe {
     private Notes notes;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+        joinColumns = @JoinColumn(name = "recipe_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
