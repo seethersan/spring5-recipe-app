d7d286fa306ef44e400d8a733b4c2780e7636e5b
seethersan <carlos_jcez@hotmail.com>
2020-01-27 08:58:01 -0500

    added data sql

---
+++ recipe-app-data/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (name, description) VALUES ('American', 'Classic american food');
+INSERT INTO category (name, description) VALUES ('Italian', 'Classic italian food');
+INSERT INTO category (name, description) VALUES ('Mexican', 'Classic mexican food');
+INSERT INTO category (name, description) VALUES ('Fast Food', 'Classic fast food');
+INSERT INTO unit_of_measure (name) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (name) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (name) VALUES ('Cup');
+INSERT INTO unit_of_measure (name) VALUES ('Pinch');
+INSERT INTO unit_of_measure (name) VALUES ('Ounce');
